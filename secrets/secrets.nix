d5f5b8d302210ea82ae447b3ba3ac3b3592e60e1
marques-jpg <gfelicianomarques@gmail.com>
2026-05-01 20:56:03 +0100

    feat: fix secrets.nix for my desktop

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,6 @@
 let
   marques = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0tAycY/GdCAVHvOfakuy3kS+SFdfb1m239FWf3s720 gfelicianomarques@gmail.com";
+  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9YtKxJEmXXvIoWaPqMr06z1LYMuRWt9+Uu4LQEQoFV root@nixos";
 in
 {
   "teste.age".publicKeys = [ marques ];
